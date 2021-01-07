d79d6d7ecf0af09deff0af5515c92b37476091c8
Jason Zhou <jzhou35@buffalo.edu>
2021-01-07 15:07:52 -0500

    Fixed random error

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
     has_many :student_courses, dependent: :destroy
     has_many :students, through: :student_courses
-    has_many :course_courses
+    has_many :school_courses
     has_many :schools, through: :school_courses
 
     validates :name, presence: true 
